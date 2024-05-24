42dabcfd5225c9e9fb1b75080a21a466f769eca1
Aritra Sen <aritra.sen@contenterra.com>
2024-05-24 11:54:08 +0530

    added attachment_types entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { VendorModule } from './Modules/vendor/vendor.module';
-import { TypeOrmSharedModule } from './common/db/typeorm.module';
+import { TypeOrmSharedModule } from './common/config/db/typeorm.module';
 import { APP_FILTER } from '@nestjs/core';
 import { AllExceptionFilter } from './common/filters';
-import { InvoiceDetailsModule } from './modules/invoice_details/invoice_details.module';
 import { InvoiceModule } from './modules/invoice/invoice.module';
-import { InvoiceHeaderModule } from './modules/invoice_header/invoice_header.module';
 import { UserRoleModule } from './modules/user-roles/user_roles.module';
 
 @Module({
@@ -24,8 +22,6 @@ import { UserRoleModule } from './modules/user-roles/user_roles.module';
 
     // * Import Custom Modules
     InvoiceModule,
-    InvoiceHeaderModule,
-    InvoiceDetailsModule,
     UserRoleModule,
     VendorModule,
   ],
