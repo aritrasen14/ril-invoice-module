c4f7ec31ac70efad07f415fb665395a6a472d9d0
Aritra Sen <aritra.sen@contenterra.com>
2024-05-24 11:28:13 +0530

    make the response-properties readonly

--- src/common/dtos/entity_response.dto.ts
+++ src/common/dtos/entity_response.dto.ts
@@ -5,14 +5,14 @@ export class EntityResponseDto {
     example: 'cb9f2c82-2910-4766-a634-54502dfb01e1',
     format: 'v4',
   })
-  public id: string;
+  readonly id: string;
 
   @ApiResponseProperty()
-  public created_at: Date;
+  readonly created_at: Date;
 
   @ApiResponseProperty()
-  public updated_at: Date;
+  readonly updated_at: Date;
 
   @ApiResponseProperty()
-  public is_active: boolean;
+  readonly is_active: boolean;
 }
