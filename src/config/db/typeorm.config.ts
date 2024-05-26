a8b7885a463ae5131f511a5babd3d8781d7bfeda
Aritra Sen <aritra.sen@contenterra.com>
2024-05-26 13:03:12 +0530

    define TypeOrmConfig

--- src/config/db/typeorm.config.ts
+++ src/config/db/typeorm.config.ts
@@ -4,25 +4,31 @@ import {
   TypeOrmModuleOptions,
 } from '@nestjs/typeorm';
 
+export default class TypeOrmConfig {
+  static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: configService.get<string>('POSTGRES_HOST'),
+      port: +configService.get<string>('POSTGRES_PORT'),
+      username: configService.get<string>('POSTGRES_USERNAME'),
+      password: configService.get<string>('POSTGRES_PASSWORD'),
+      database: configService.get<string>('POSTGRES_DATABASE'),
+      schema: configService.get<string>('POSTGRES_DATABASE_SCHEMA'),
+      // entities: [
+      // __dirname + '/**/*.entity{.ts,.js}'
+      // ],
+      autoLoadEntities: true,
+      synchronize: true,
+      migrations: [],
+      logging: ['error', 'info', 'log', 'query', 'schema', 'warn', 'migration'],
+    };
+  }
+}
+
 export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
   useFactory: async (
     configService: ConfigService,
-  ): Promise<TypeOrmModuleOptions> => ({
-    type: 'postgres',
-    host: configService.get<string>('POSTGRES_HOST'),
-    port: +configService.get<string>('POSTGRES_PORT'),
-    username: configService.get<string>('POSTGRES_USERNAME'),
-    password: configService.get<string>('POSTGRES_PASSWORD'),
-    database: configService.get<string>('POSTGRES_DATABASE'),
-    schema: configService.get<string>('POSTGRES_DATABASE_SCHEMA'),
-    // entities: [
-    // __dirname + '/**/*.entity{.ts,.js}'
-    // ],
-    autoLoadEntities: true,
-    synchronize: true,
-    migrations: [],
-    logging: ['error', 'info', 'log', 'query', 'schema', 'warn', 'migration'],
-  }),
+  ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
   inject: [ConfigService],
 };
